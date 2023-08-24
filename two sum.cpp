8f74e2520f93e9840674929b13c321bccf1293de
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-08-24 10:37:45 +0530

    Update and rename two sum to two sum.cpp

---
+++ two sum.cpp
@@ -0,0 +1,31 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a = nums.size();
+        for(int i=0; i<a-1;i++){
+            for(int j=i+1; j<a;j++){
+                if(nums[i]+nums[j] == target){
+                    return{i,j};
+                }
+            }
+        }
+        return {};
+    }
+    
+};
