644656f53d387bebe92c5c81a10d1b9e8b2e5e92
Kunal453 <103109790+Kunal453@users.noreply.github.com>
2023-09-23 14:54:14 +0530

    Create Evaluate Reverse Polish Notation.cpp

---
+++ Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,75 @@
+/*
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+
+Constraints:
+
+1 <= tokens.length <= 104
+tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+  */
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto x: tokens){
+            if(x == "+" || x == "-" || x == "/" || x =="*"){
+                int op2 = st.top(); st.pop();
+                int op1 = st.top(); st.pop();
+
+                if(x == "+"){
+                    st.push(op1+op2);
+                }
+                if(x == "-"){
+                    st.push(op1-op2);
+                }
+                if(x == "/"){
+                    st.push(op1/op2);
+                }
+                if(x == "*"){
+                    st.push(op1*op2);
+                }
+            }
+            else{
+                stringstream ss(x);
+                int data ;
+                ss >> data;
+                st.push(data);
+            }
+        }
+        return st.top();
+    }
+};
